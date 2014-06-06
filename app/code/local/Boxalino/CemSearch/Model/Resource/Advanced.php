a587a4f59ba3328f037781b87a20ca984daecf88
Szymon Nosal <szymon.nosal@codete.co>
2014-06-06 09:06:43 +0200

    Add Advanced search

---
+++ app/code/local/Boxalino/CemSearch/Model/Resource/Advanced.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by: Szymon Nosal <szymon.nosal@codete.com>
+ * Created at: 05.06.14 13:57
+ */
+
+require_once "Mage/CatalogSearch/Model/Resource/Advanced";
+
+class Boxalino_CemSearch_Model_Resource_Advanced extends Mage_CatalogSearch_Model_Resource_Advanced{
+
+}
