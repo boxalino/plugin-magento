0c1dcdb5b3df4660b9f1f879fe29dab4d7afb57a
Michal Sordyl <sordyl.michal@gmail.com>
2014-06-09 09:13:18 +0200

    Removed old 'test' files and old config. Moved search setting to 'Catalog' tab

--- app/code/local/Boxalino/CemSearch/Model/Mysql4/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	class Boxalino_CemSearch_Model_Mysql4_Test extends Mage_Core_Model_Mysql4_Abstract{
-		public function _construct(){
-			$this->_init('test/test', 'id_boxalino_person');
-		}
-	}
-
-?>
