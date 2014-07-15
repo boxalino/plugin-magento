6c32f76b68d3c3ea487c47c8de13f8c0d219f789
Andrzej Wojtyś <andrze.wojtys@codete.co>
2014-07-15 18:45:58 +0200

    deleted old module

--- app/code/local/Boxalino/Export/Model/Mysql4/Product.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Boxalino_Export_Model_Mysql4_Product extends Mage_Catalog_Model_Resource_Eav_Mysql4_Product {
-	public function __construct() {
-		parent::__construct();
-
-        $this->setType('catalog_product');
-    }
-}
