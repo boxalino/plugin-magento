8e62318c84817671c5a476ce6db78237e5a6af6f
Andrzej Wojtyś <andrze.wojtys@codete.co>
2014-09-03 16:24:07 +0200

    clean of master

--- app/code/local/Boxalino/Manager/controllers/Adminhtml/FieldController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Boxalino_Manager_Adminhtml_FieldController extends Mage_Adminhtml_Controller_Action
-{
-    public function indexAction()
-    {
-        $this->loadLayout()->renderLayout();
-    }
-}
