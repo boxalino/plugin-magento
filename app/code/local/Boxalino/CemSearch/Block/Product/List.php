b34410dc5ece21852360a2b5f574103b14e7fb97
Simon Rupf <simon.rupf@boxalino.com>
2015-03-02 17:09:02 +0100

    adding checks to suppress warnings and don't load the product collection by default

--- app/code/local/Boxalino/CemSearch/Block/Product/List.php
+++ app/code/local/Boxalino/CemSearch/Block/Product/List.php
@@ -10,27 +10,27 @@ class Boxalino_CemSearch_Block_Product_List extends Mage_Catalog_Block_Product_L
     protected function _getProductCollection()
     {
         if (Mage::getStoreConfig('Boxalino_General/general/enabled', 0) == 0) {
-        	return parent::_getProductCollection();
+            return parent::_getProductCollection();
         }
 
-        $entity_ids = Mage::helper('Boxalino_CemSearch')->getSearchAdapter()->getEntitiesIds();
+        if (is_null($this->_productCollection)) {
+            $entity_ids = Mage::helper('Boxalino_CemSearch')->getSearchAdapter()->getEntitiesIds();
 
-        $this->_productCollection = Mage::getResourceModel('catalog/product_collection')
-             ->addFieldToFilter('entity_id', $entity_ids)
-             ->addAttributeToSelect('*');
+            $this->_productCollection = Mage::getResourceModel('catalog/product_collection');
 
-        if (Mage::helper('catalog')->isModuleEnabled('Mage_Checkout')) {
-            Mage::getResourceSingleton('checkout/cart')->addExcludeProductFilter($this->_productCollection,
-                Mage::getSingleton('checkout/session')->getQuoteId()
-            );
-            $this->_addProductAttributesAndPrices($this->_productCollection);
-        }
-        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($this->_productCollection);
+            // Added check if there are any entity ids
+            if(count($entity_ids) > 0){
+                $this->_productCollection->addFieldToFilter('entity_id', $entity_ids)
+                    ->addAttributeToSelect('*');
+            }
 
-        $this->_productCollection->load();
-
-        foreach ($this->_productCollection as $product) {
-            $product->setDoNotUseCategoryId(true);
+            if (Mage::helper('catalog')->isModuleEnabled('Mage_Checkout')) {
+                Mage::getResourceSingleton('checkout/cart')->addExcludeProductFilter($this->_productCollection,
+                    Mage::getSingleton('checkout/session')->getQuoteId()
+                );
+                $this->_addProductAttributesAndPrices($this->_productCollection);
+            }
+            Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($this->_productCollection);
         }
 
         return $this->_productCollection;
--- app/code/local/Boxalino/CemSearch/Helper/P13n/Recommendation.php
+++ app/code/local/Boxalino/CemSearch/Helper/P13n/Recommendation.php
@@ -20,7 +20,7 @@ class Boxalino_CemSearch_Helper_P13n_Recommendation
 
     public function getRecommendation($widgetType, $widgetName)
     {
-        if(empty($this->results)) {
+        if (empty($this->results)) {
             $widgets = $this->prepareWidgets($widgetType);
             if (empty($widgets)) {
                 return null;
@@ -51,6 +51,12 @@ class Boxalino_CemSearch_Helper_P13n_Recommendation
             $p13nClient->createRecommendation($account, $authData, $language, $entityIdFieldName, true);
             $this->results = $p13nClient->getPersonalRecommendations($widgets, $returnFields, $widgetType);
         }
+
+        // Added check in order to avoid PHP notice
+        if (!array_key_exists($widgetName, $this->results)) {
+            return null;
+        }
+
         return $this->results[$widgetName];
     }
 
@@ -58,13 +64,14 @@ class Boxalino_CemSearch_Helper_P13n_Recommendation
     {
         $widgets = array();
         $recommendations = Mage::getStoreConfig('Boxalino_Recommendation');
-        foreach($recommendations as $recommendation) {
-            if(
+        foreach ($recommendations as $recommendation) {
+            if (
                 (!empty($recommendation['min']) || $recommendation['min'] >= 0) &&
                 (!empty($recommendation['max']) || $recommendation['max'] >= 0) &&
                 !empty($recommendation['scenario']) &&
                 ($recommendation['min'] <= $recommendation['max']) &&
-                $recommendation['status'] == true) {
+                $recommendation['status'] == true
+            ) {
                 if ($recommendation['scenario'] == $widgetType) {
                     $widgets[] = array('name' => $recommendation['widget'], 'min_recs' => $recommendation['min'], 'max_recs' => $recommendation['max']);
                 }
